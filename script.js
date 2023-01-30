9c3d79b5b47436b3516cc0efb397bb5eaaafb6fb
Luke Spicknall <lukejspicknall@gmail.com>
2023-01-30 00:29:52 -0500

    add some intial custom js form validation

--- script.js
+++ script.js
@@ -1,14 +1,55 @@
-let firstName = document.getElementById('first-name');
-// const email = document.getElementById("email");
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
 // const emailError = document.querySelector('#email + span.error')
 // const form = document.querySelector("form")
 const pword = document.getElementById("pword");
-notValidate();
+// notValidate();
 
-function notValidate () {
-    if (firstName.value === "M") {
+firstName.addEventListener("input", (e) => {
+    if (firstName.validity.tooShort) {
         firstName.className = 'notValid'
+        firstName.setCustomValidity("We need your name.");
     } else {
-        firstName.className = 'definput'
+        firstName.className = 'isValid'
+        firstName.setCustomValidity("");
     }
-} 
+});
+
+lastName.addEventListener("input", (e) => {
+    if (lastName.validity.tooShort) {
+        lastName.className = 'notValid'
+        lastName.setCustomValidity("We need your name.");
+    } else {
+        lastName.className = 'isValid'
+        lastName.setCustomValidity("");
+    }
+});
+
+email.addEventListener("input", (e) => {
+    if (email.validity.typeMismatch) {
+        email.className = 'notValid'
+        email.setCustomValidity("Must be in email form.");
+    } else {
+        email.className = 'isValid'
+        email.setCustomValidity("");
+    }
+});
+
+phone.addEventListener("input", (e) => {
+    if (phone.validity.typeMismatch) {
+        phone.className = 'notValid'
+        phone.setCustomValidity("Must be in phone form.");
+    } else {
+        phone.className = 'isValid'
+        phone.setCustomValidity("");
+    }
+});
+// function notValidate () {
+//     if (firstName.validityState.tooshort) {
+//         firstName.className = 'notValid'
+//     } else {
+//         firstName.className = 'definput'
+//     }
+// } 
